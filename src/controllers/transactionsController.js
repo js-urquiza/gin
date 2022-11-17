4c34505d363183cea1610b4da99628c36aa39ff7
juansantiago <js.urquiza@gmail.com>
2022-11-17 09:49:24 -0300

    Ordenamiento rutas y controladores

--- src/controllers/transactionsController.js
+++ src/controllers/transactionsController.js
@@ -3,34 +3,54 @@ const db = require("../database/models");
 const aux = require('../utils/functions');
 
 module.exports = {
-  create: async function(req, res) { //No se está usando
-    
+  createRent: async function (req, res) {
+    //No se está usando
+
     let fecha = req.body.initialDate;
     let duracion = req.body.duration;
     let precio = req.body.amount;
     let contrato = req.session.contractIdInUse;
-    
+
     try {
-       aux.createPeriodicRent(fecha, duracion, precio, contrato);
+      aux.createPeriodicRent(fecha, duracion, precio, contrato);
     } catch (error) {
       console.error(error);
     }
-    
-    
 
-    res.redirect('back');
-
+    res.redirect("back");
   },
 
-  destroy: async function(req, res) {
-    
+  createExpense: async function (req, res) {
+    let fecha = req.body.initialDate;
+    let duracion = req.body.duration;
+    let nombre = req.body.name;
+    let precio = req.body.amount;
+    let coeficiente = req.body.coeff;
+    let contrato = req.session.contractIdInUse;
+
+    try {
+      aux.createPeriodicExpense(
+        fecha,
+        duracion,
+        nombre,
+        precio,
+        coeficiente,
+        contrato
+      );
+    } catch (error) {
+      console.error(error);
+    }
+
+    res.redirect("back");
+  },
+
+  destroy: async function (req, res) {
     await db.Transactions.destroy({
       where: {
-        id: req.params.id
-      }
-    })
+        id: req.params.id,
+      },
+    });
 
-    res.redirect('back');
-
-  }
+    res.redirect("back");
+  },
 };
--- src/routes/transactions.js
+++ src/routes/transactions.js
@@ -3,7 +3,6 @@ const router = express.Router();
 
 const transactionsController = require("../controllers/transactionsController");
 
-router.post("/create", transactionsController.create);
 router.delete("/:id/delete", transactionsController.destroy);
 
 module.exports = router;
